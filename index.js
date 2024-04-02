630d311de5a4bfe7c126e38176d1f167cd5647c9
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-04-01 22:28:29 -0400

    completed

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+if (num<=399){
+   return ("This one is on me!")
+}
+else if (num>=400&&num<2000) {
+  return ("That will be twenty bucks.")
+}
+else if (num>=2000&&num<2500){
+  return ("I will gladly take your thirty bucks.")
+}
+else if (num>=2500) {
+  return ("No can do.")
+}
+}
+function ternaryCheckCity(isCityNyc){
+ return isCityNyc==="NYC"?'Ok, sounds good.': 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){ 
+let response= ""
+switch (tip){
+  case "generous":
+    response="Thank you so much."
+  return response
 }
+switch (tip){
+ case "not as generous":
+  response = "Thank you."
+  return response
+  default:
+    response = "Bye.";
+    return response
+}
+}
+ 
+
